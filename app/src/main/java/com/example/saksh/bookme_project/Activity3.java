0caa06805437a13c4904700681289c0806938b70
saksham91 <38701227+saksham91@users.noreply.github.com>
2018-11-25 17:11:00 -0500

    Activity 4 designed and programmed

--- app/src/main/java/com/example/saksh/bookme_project/Activity3.java
+++ app/src/main/java/com/example/saksh/bookme_project/Activity3.java
@@ -3,10 +3,26 @@ package com.example.saksh.bookme_project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Activity3 extends AppCompatActivity{
+    Spinner spinner;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+
+        spinner = findViewById(R.id.spinner);
+        String[] num_of_tickets = {"1", "2", "3", "4", "5", "6", "7"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, num_of_tickets);
+        spinner.setAdapter(adapter);
+    }
+
+    public void nextActivity4(View view){
+        Intent intent = new Intent(Activity3.this, Activity4.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/saksh/bookme_project/Activity4.java
@@ -0,0 +1,12 @@
+package com.example.saksh.bookme_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Activity4 extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_4);
+    }
+}
